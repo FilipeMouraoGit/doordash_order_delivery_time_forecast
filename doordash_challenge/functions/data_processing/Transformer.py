9442893ac91d4210c1c9de8cde08b6b47c6fae49
Filipe Assis Mourão <filipemouraodsgit@gmail.com>
2024-11-18 17:37:12 -0300

    Add relevant ranks

--- doordash_challenge/functions/data_processing/Transformer.py
+++ doordash_challenge/functions/data_processing/Transformer.py
@@ -1,7 +1,7 @@
 import logging
 from typing import List
 import pandas as pd
-
+import numpy as np
 logging.basicConfig(level=logging.INFO)
 
 ALLOWED_METRICS = {
@@ -22,13 +22,43 @@ class DataTransformer:
         return grouped_data
 
     @staticmethod
-    def generate_cumulative_time_series(data, date_column, metric='revenue'):
+    def validate_metric_and_group(data, column, metric):
         if metric not in list(ALLOWED_METRICS.keys()):
             raise ValueError(f'Metric `{metric}` not supported')
         grouped_data = DataTransformer.group_data(
-            data, group_columns=[date_column], agg_methods={'metric': ALLOWED_METRICS[metric]}
+            data, group_columns=[column], agg_methods={'metric': ALLOWED_METRICS[metric]}
         )
+        return grouped_data
+    @staticmethod
+    def get_column_totals(data, total_columns: List):
+        """
+        Given the `data` df group the data with the columns in the `group_columns` list calculating the metrics
+        passed in the metrics dictionary with the logic {column_name:['column_to_be_calculated','operation']} ex:
+        {'total_spend':['subtotal','sum'], 'n_transactions':['created_at','count'], 'total_items':['items','sum']}
+        """
+        total_dict = {}
+        for column in total_columns:
+            total_dict[column] = data[column].sum()
+        return total_dict
+
+    @staticmethod
+    def generate_cumulative_time_series(data, date_column, metric='revenue'):
+        grouped_data = DataTransformer.validate_metric_and_group(data, date_column, metric)
         grouped_data = grouped_data.sort_values(date_column)
         grouped_data['cum_metric'] = grouped_data['metric'].cumsum()
         return grouped_data
+    @staticmethod
+    def generate_percentage_grop(data, column, metric='revenue'):
+        grouped_data = DataTransformer.validate_metric_and_group(data, column, metric)
+        grouped_data = grouped_data.set_index(column)
+        percentage_df = 100 * np.round(grouped_data / grouped_data.sum(), 2)
+        percentage_df = percentage_df.reset_index()
+        return percentage_df
+
+    @staticmethod
+    def generate_rank_group(data, column, metric='revenue', rank=5):
+        grouped_data = DataTransformer.validate_metric_and_group(data, column, metric)
+        grouped_data = grouped_data.sort_values('metric')
+        top_data = grouped_data.tail(rank)
+        return top_data
 
--- doordash_challenge/functions/data_processing/Viewer.py
+++ doordash_challenge/functions/data_processing/Viewer.py
@@ -47,6 +47,75 @@ class DataViewer:
             template='plotly_dark')
         fig.update_yaxes(tickformat=",")
         return fig
-    
+    @staticmethod
+    def plot_percentage_distribution(
+            data: pd.DataFrame,
+            column: str,
+            metric: str,
+            order_values: List,
+            title: str,
+            color: str
+    ):
+        """
+        Given the `data` df group the data with the columns in the `group_columns` list calculating the metrics
+        passed in the metrics dictionary with the logic {column_name:['column_to_be_calculated','operation']} ex:
+        {'total_spend':['subtotal','sum'], 'n_transactions':['created_at','count'], 'total_items':['items','sum']}
+        """
+        percentage_df = DataTransformer.generate_percentage_grop(data, column, metric)
+        fig = px.bar(
+            percentage_df,
+            y=column,
+            x='metric',
+            category_orders={column: order_values},
+            orientation='h',
+            color_discrete_sequence=[color]
+        )
+        fig.update_yaxes(type='category')
+        fig.update_traces(texttemplate='%{x:.2s}%', textposition='outside')
+        fig.update_layout(
+            title={'text': title, 'x': 0.45, 'xanchor': 'center'},
+            xaxis_title={'text': None},
+            yaxis_title={'text': None},
+            template='plotly_dark',
+            xaxis={'showticklabels': False},
+        )
+        return fig
+
+    @staticmethod
+    def plot_bar_rank(
+        data: pd.DataFrame,
+        column: str,
+        metric: str,
+        title: str,
+        color: str,
+        rank: int,
+    ):
+        """
+        Given the `data` df group the data with the columns in the `group_columns` list calculating the metrics
+        passed in the metrics dictionary with the logic {column_name:['column_to_be_calculated','operation']} ex:
+        {'total_spend':['subtotal','sum'], 'n_transactions':['created_at','count'], 'total_items':['items','sum']}
+        """
+        rank_df = DataTransformer.generate_rank_group(data, column, metric, rank)
+        convert_to_str_with_sep = np.vectorize(lambda x: f"{x:,}")
+        text_legend = convert_to_str_with_sep(np.round(rank_df['metric'], 1))
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            y=rank_df[column],
+            x=rank_df['metric'],
+            text=text_legend,
+            textposition='outside',
+            marker_color=color,
+            orientation='h'
+        ))
+        fig.update_yaxes(type='category')
+        fig.update_traces(textposition='outside')
+        fig.update_layout(
+            title={'text': title, 'x': 0.45, 'xanchor': 'center'},
+            xaxis_title={'text': None},
+            yaxis_title={'text': None},
+            template='plotly_dark',
+            xaxis={'showticklabels': False},
+        )
+        return fig
 
 
